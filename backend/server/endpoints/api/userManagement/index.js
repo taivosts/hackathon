9e86d9e3186cf35489aed5f5285e43f6aefc8e1b
Vut <vu.truong@saigontechnology.com>
2025-03-25 10:34:12 +0700

    add API get workspaces for user

--- backend/server/endpoints/api/userManagement/index.js
+++ backend/server/endpoints/api/userManagement/index.js
@@ -5,6 +5,7 @@ const {
   simpleSSOEnabled,
 } = require("../../../utils/middleware/simpleSSOEnabled");
 const { validApiKey } = require("../../../utils/middleware/validApiKey");
+const { Workspace } = require("../../../models/workspace");
 
 function apiUserManagementEndpoints(app) {
   if (!app) return;
@@ -63,6 +64,128 @@ function apiUserManagementEndpoints(app) {
       response.sendStatus(500).end();
     }
   });
+
+  app.get(
+    "/v1/user/:id", [validApiKey], async (request, response) => {
+      /*
+      #swagger.tags = ['User Management']
+      #swagger.description = 'Get user by ID'
+      #swagger.parameters['id'] = {
+        in: 'path',
+        description: 'The ID of the user to retrieve',
+        required: true,
+        type: 'string'
+      }
+      #swagger.responses[200] = {
+        content: {
+          "application/json": {
+            schema: {
+              type: 'object',
+              example: {
+                id: 1,
+                username: "john_doe",
+                role: "admin"
+              }
+            }
+          }
+        }
+      }
+    #swagger.responses[403] = {
+      schema: {
+        "$ref": "#/definitions/InvalidAPIKey"
+      }
+    }
+     #swagger.responses[401] = {
+      description: "Instance is not in Multi-User mode. Permission denied.",
+    }
+      */
+      try {
+        if (!multiUserMode(response))
+          return response
+            .status(401)
+            .send("Instance is not in Multi-User mode. Permission denied.");
+
+        const { id: userId } = request.params;
+        const user = await User.get({ id: Number(userId) });
+        if (!user)
+          return response.status(404).json({ error: "User not found" });
+
+        response.status(200).json(user);
+      } catch (e) {
+        console.error(e.message, e);
+        response.sendStatus(500).end();
+      }
+    }
+  )
+
+  app.get(
+    "/v1/user/:id/workspaces",
+    [validApiKey],
+    async (request, response) => {
+      /*
+      #swagger.tags = ['User Management']
+      #swagger.description = 'Get workspaces for a user by ID'
+      #swagger.parameters['id'] = {
+        in: 'path',
+        description: 'The ID of the user to retrieve workspaces for',
+        required: true,
+        type: 'string'
+      }
+      #swagger.responses[200] = {
+        content: {
+          "application/json": {
+            schema: {
+              type: 'object',
+              example: {
+                workspaces: [
+                  {
+                    "id": 1,
+                    "name": "Workspace 1"
+                  },
+                  {
+                    "id": 2,
+                    "name": "Workspace 2"
+                  }
+                ]
+              }
+            }
+          }
+        }
+      }
+    #swagger.responses[403] = {
+      schema: {
+        "$ref": "#/definitions/InvalidAPIKey"
+      }
+    }
+     #swagger.responses[401] = {
+      description: "Instance is not in Multi-User mode. Permission denied.",
+    }
+      */
+      try {
+        if (!multiUserMode(response))
+          return response
+            .status(401)
+            .send("Instance is not in Multi-User mode. Permission denied.");
+
+        const { id: userId } = request.params;
+        const user = await User.get({ id: Number(userId) });
+        if (!user)
+          return response.status(404).json({ error: "User not found" });
+
+        const workspaces = await Workspace.get({
+          workspace_users: {
+            some: {
+              user_id: user?.id,
+            },
+          }
+        });
+        response.status(200).json({ workspaces });
+      } catch (e) {
+        console.error(e.message, e);
+        response.sendStatus(500).end();
+      }
+    }
+  )
 
   app.get(
     "/v1/users/:id/issue-auth-token",
